fef2595a563883b96bd6193b903270a0aa76f961
Avinash Prasad <avinashprasad4608@gmail.com>
2024-08-10 12:32:18 +0530

    add image

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from "../Components/images.jpg"
 
 const NewsItem = (props)=> {
         let { title, description, imageUrl, newsUrl, author, date, source } = props;
@@ -14,7 +15,7 @@ const NewsItem = (props)=> {
                     }> 
                         <span className="badge rounded-pill bg-danger"> {source} </span>
                     </div>
-                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <img src={!imageUrl ? Image : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}  </h5>
                         <p className="card-text">{description}</p>
